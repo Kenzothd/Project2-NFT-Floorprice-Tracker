3c5cc68482360427cfc68f94376de784c1aee372
Kenzothd <Kenzothd@hotmail.com>
2022-08-23 23:39:35 +0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Test</h1>
+    </div>
+  );
 }
 
 export default App;
